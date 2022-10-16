8583c00ae5e7a54b26285e59a4b679b3979f3da6
rahulsen200886 <55349693+rahulsen200886@users.noreply.github.com>
2022-10-16 13:44:33 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ Press 2 EU
 read region
 echo "Now I am creating your custom cluster and setting up the ecosystem .. .. "
 
-gcloud beta container  --project "${DEVSHELL_PROJECT_ID}" clusters create-auto "example" --region "us-central1" --no-enable-basic-auth 
+gcloud beta container  --project "${DEVSHELL_PROJECT_ID}" clusters create-auto "example" --region "us-central1" 
 gsutil mb gs://${DEVSHELL_PROJECT_ID}-nas-bucket
 sed -i "s/NAS-BUK/${DEVSHELL_PROJECT_ID}-nas-bucket/g" gkeyml/nas.yaml
 gcloud iam service-accounts create smbnfsshare-sa --display-name="My Custom Service Account"
