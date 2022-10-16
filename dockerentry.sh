ae69769bc60955857f49d8bbd994f4f1c1c01063
rahulsen200886 <55349693+rahulsen200886@users.noreply.github.com>
2022-10-16 17:52:11 +0530

    Update dockerentry.sh

--- dockerentry.sh
+++ dockerentry.sh
@@ -2,7 +2,6 @@
 echo "Running startup"
 export GOOGLE_APPLICATION_CREDENTIALS=/accounts/key.json
 echo "Running fuse"
-gcsfuse -o rw,allow_other ${BUCKET} /export
 echo -e "
 [export]
  path = /export
@@ -13,4 +12,5 @@ echo -e "
  force user = mysmbuser
  force group = smbgroup
 " >> /etc/samba/smb.conf
+gcsfuse -o rw,allow_other ${BUCKET} /export
 smbd -F
