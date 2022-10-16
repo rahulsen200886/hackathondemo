a7750e6529c70427a7fa841ad15917db74fddb6d
rahulsen200886 <55349693+rahulsen200886@users.noreply.github.com>
2022-10-16 10:59:47 +0530

    Update dockerentry.sh

--- dockerentry.sh
+++ dockerentry.sh
@@ -1,5 +1,5 @@
 echo "Running startup"
-yum -y update ; yum clean all
+yum -y update 
 yum -y install gcsfuse samba-common-tools realmd oddjob oddjob-mkhomedir sssd adcli krb5-workstation nscd
 export GOOGLE_APPLICATION_CREDENTIALS=/accounts/key.json
 echo "Running fuse"
